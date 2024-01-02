5fa956487bfbdd1564677baca248f5dd037b2fd6
Lucas Matias <101410443+MatiasDeLima@users.noreply.github.com>
2024-01-02 18:45:40 -0300

    feat(04): Finalize webpack lesson

--- 03-basic-scene/main.js
+++ 03-basic-scene/main.js
@@ -11,7 +11,7 @@ console.log(scene);
 
 // Objects
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.BasicMeshMaterial({ color: 0xff0000 });
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
