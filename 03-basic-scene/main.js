54cd399893f2659ba17fab7f8c8b42f73b2349ce
Lucas Matias <101410443+MatiasDeLima@users.noreply.github.com>
2024-01-02 17:06:35 -0300

    feat(03): basic scene add completed

--- 03-basic-scene/main.js
+++ 03-basic-scene/main.js
@@ -11,13 +11,24 @@ console.log(scene);
 
 // Objects
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: FFF});
+const material = new THREE.BasicMeshMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
 // Sizes
-
+const sizes = {
+    width: 800,
+    height: 600
+};
 
 // Camera
-
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 4
+scene.add(camera);
 
 // Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
