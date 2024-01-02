d93b1f9ec3b8507fb746aaa8d772719b45e84937
Lucas Matias <101410443+MatiasDeLima@users.noreply.github.com>
2024-01-02 16:30:47 -0300

    feat(03): lesson 03 basic scene

---
+++ 03-basic-scene/main.js
@@ -0,0 +1,23 @@
+console.log("Hello Three js");
+console.log(THREE);
+
+// Canvas
+const canvas = document.querySelector(".canvas");
+console.log(canvas);
+
+// Scene
+const scene = new THREE.Scene();
+console.log(scene);
+
+// Objects
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: FFF});
+const mesh = new THREE.Mesh(geometry, material);
+
+// Sizes
+
+
+// Camera
+
+
+// Renderer
